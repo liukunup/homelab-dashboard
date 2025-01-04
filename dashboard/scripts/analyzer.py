6e5fa48c4ad0f948e848ef7716d550790e7933e8
Liu Kun <liukunup@163.com>
2025-01-04 22:47:25 +0800

    完成标签的导入导出

--- dashboard/scripts/analyzer.py
+++ dashboard/scripts/analyzer.py
@@ -5,6 +5,7 @@
 # description: 数据分析器
 
 import os
+import json
 import typing
 import argparse
 from sqlalchemy import create_engine, text
@@ -86,6 +87,85 @@ class FrequencyAnalyzer(AbstractAnalyzer):
         print('-' * 100)
 
 
+class PreMarkAnalyzer(AbstractAnalyzer):
+    """ 预标记分析器 """
+
+    def analyze(self, **kwargs):
+        # 打印`kwargs`参数
+        print(f'辅助参数: {kwargs}')
+        op = kwargs.get('op', 'import')
+        if op == 'import':
+            self.import_tags()
+        elif op == 'export':
+            self.export_tags()
+        else:
+            print(f'未知操作: {op}')
+
+    def import_tags(self, **kwargs):
+        """
+        从json文件中导入标签数据到`transaction_tag`表中
+        :return: None
+        """
+        # 读取json文件
+        with open('transaction_tag.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            # 创建数据库连接
+            with self._engine.begin() as connection:
+                # 遍历json数据
+                for field, tags in data.items():
+                    for tag, values in tags.items():
+                        if tag == '暂未标记':
+                            continue
+                        print(f'字段: {field}, 标签: {tag}, 值的数量: {len(values)}')
+                        for value in values:
+                            stmt = text('''INSERT INTO transaction_tag (field, value, counts, tag) VALUES (:field, :value, 0, :tag)
+                                           ON DUPLICATE KEY UPDATE tag = :tag_need_update, update_time = NOW()''')
+                            connection.execute(stmt, {
+                                'field': field,
+                                'value': value,
+                                'tag': tag,
+                                'tag_need_update': tag
+                            })
+        # 打印分割线
+        print('<' * 100)
+
+    def export_tags(self, **kwargs):
+        """
+        从`transaction_tag`表中导入标签数据到json文件中
+        :return: None
+        """
+        white_list = ['category', 'counterparty', 'goods', 'income_or_expenditure', 'channel']
+        black_list = ['account']
+        # 创建数据库连接
+        with self._engine.begin() as connection:
+            # 查询标签数据
+            query = text('SELECT `field`, `value`, `tag` FROM dashboard.transaction_tag')
+            results = connection.execute(query)
+            # 遍历结果集
+            output = {}
+            for row in results:
+                field, value, tag = row[0], row[1], row[2]
+                # 跳过黑名单字段
+                if field in black_list:
+                    continue
+                # 只保留白名单字段
+                if field not in white_list:
+                    continue
+                if field not in output:
+                    output[field] = {tag: [value]}
+                else:
+                    if tag not in output[field]:
+                        output[field][tag] = [value]
+                    else:
+                        output[field][tag].append(value)
+            # 将标签数据写入json文件
+            with open('transaction_tag.json', 'w', encoding='utf-8') as file:
+                file.write(json.dumps(output, ensure_ascii=False, indent=4))
+                print(f'标签数据已导出到文件: transaction_tag.json')
+        # 打印分割线
+        print('>' * 100)
+
+
 def args_parser():
     """
     命令行参数解析
@@ -97,7 +177,7 @@ def args_parser():
     parser.add_argument('--username', type=str)
     parser.add_argument('--password', type=str)
     parser.add_argument('--database', type=str, default='dashboard')
-    parser.add_argument('--type', type=str, default='Frequency', choices=['Frequency'])
+    parser.add_argument('--type', type=str, default='Frequency', choices=['Frequency', 'PreMark'])
     return parser.parse_args()
 
 
@@ -119,6 +199,13 @@ def app():
             'name': '频次分析器',
             'class': FrequencyAnalyzer,
         },
+        'PreMark': {
+            'name': '预标记分析器',
+            'class': PreMarkAnalyzer,
+            'kwargs': {
+                'op': 'export',
+            }
+        }
     }[args.type]
     print(f'[{name}] 当前分析器: {operator["name"]}')
     print('-' * 100)
